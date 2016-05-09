2876fdeaadf73f9a8e42511fd561e171fa440ff0
Sebastien Besson <seb.besson@gmail.com>
2016-05-09 22:47:33 +0100

    Re-organize project and add top-level pom

--- build.sh
+++ build.sh
@@ -35,7 +35,6 @@
 # build.sh - A script for use by continuous integration systems,
 #            which generates and builds the Bio-Formats C++ bindings.
 
-cd "$(dirname "$0")"/..
 set -ex
 mvn -DskipTests clean package cppwrap:wrap dependency:copy-dependencies
 cd target/cppwrap
